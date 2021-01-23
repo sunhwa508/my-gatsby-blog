ae90212a5065d5ca7a883d5c612cfed4d43b5730
sunhwa lee <2017sunhwa@gmail.com>
2021-01-24 00:40:20 +0900

    update readme fixed deploy from netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sunhwa Blog`,
+    description: `Sunhwa Blog`,
+    author: `@sunhwajs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,7 +13,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
--- src/markdown-pages/first-post.md
+++ src/markdown-pages/first-post.md
@@ -1,9 +1,9 @@
 ---
-title: "My First AMA"
-date: "2017-08-21"
-description: "My first AMA :)"
+title: "My First video"
+date:  "2021-01-21"
+description: "My first Video :)"
 ---
 
 Thank you everyone who enjoyed the AMA! It was my first one and I'd love to do it again. 
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/DXJO3AraeMQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/-1mJGh627R4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
